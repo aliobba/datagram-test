f4b0a8cbc53b66a1e848c450618ef9fcce61fea2
aliobba <ali.obba@esprit.tn>
2021-11-22 17:10:18 +0100

    fix add product

--- src/pages/products/AddProducts.js
+++ src/pages/products/AddProducts.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Container from '@material-ui/core/Container';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import CircularProgress from '@mui/material/CircularProgress';
 
 ///import { v4 as uuidv4 } from 'uuid';
 
@@ -27,36 +28,50 @@ function AddProducts(props) {
   //   { id: uuidv4(), title: '', price: '' , category: '' ,description: '',image:''},
   // ]);
 
-  
+
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [price, setPrice] = useState("");
   const [category, setCategory] = useState("");
+  const [image, setImage] = useState("");
+  const [loading, setLoading] = useState(false);
   const [selectedFile, setSelectedFile] = useState(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-     var formData = new FormData()
+    /* var formData = new FormData()
 
-  formData.append("title", title);
-  formData.append("price", price);
-  formData.append("category", category);
-  formData.append("description", description);
-  formData.append("image", selectedFile);
-  
-   
-    axios.post('https://fakestoreapi.com/products', formData).then(result => {
-     console.log("InputFields", formData);
-     console.log("ines", result);
-       
-        props.history.push("/products");
-    }).catch(err=>{
-     
+ formData.append("title", title);
+ formData.append("price", price);
+ formData.append("category", category);
+ formData.append("description", description);
+ formData.append("image", image); */
 
-       // console.log(err);
-       alert(err)
+    setLoading(true)
+    axios.post('https://fakestoreapi.com/products', {
+      title: title,
+      price: parseFloat(price),
+      description: description,
+      image: image,
+      category: category,
     })
-};
+      .then(result => {
+        //console.log("InputFields", formData);
+        //console.log({ result });
+        if (result.status === 200) {
+          setLoading(false)
+          alert('Ajout effectué avec succès')
+        }
+
+        props.history.push("/products");
+      }).catch(err => {
+
+
+        setLoading(false)
+        // console.log(err);
+        alert(err)
+      })
+  };
 
   // const handleChangeInput = (id, event) => {
   //   const newInputFields = inputFields.map(i => {
@@ -65,7 +80,7 @@ function AddProducts(props) {
   //     }
   //     return i;
   //   })
-    
+
   //   setInputFields(newInputFields);
   // }
 
@@ -75,62 +90,68 @@ function AddProducts(props) {
     <Container>
       <h1>Add New Product</h1>
       <form className={classes.root} onSubmit={handleSubmit}>
-       
-          <div>
-            <TextField
-              name="title"
-              label="title"
-              variant="filled"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-            /><br/>
-            <TextField
-              name="price"
-              label="price"
-              variant="filled"
-              value={price}
-              onChange={(e) => setPrice(e.target.value)}
-            />
-         <br/>
-         <TextField
-              name="category"
-              label="category"
-              variant="filled"
-              value={category}
-              onChange={(e) => setCategory(e.target.value)}
-            /><br/>
-             <TextField
-              name="description"
-              label="description"
-              variant="filled"
-              multiline
-              
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-            /><br/>
-           <Input accept="image/jpg, image/jpeg, image/png" type="file"  name="image" onChange={(e) => setSelectedFile(e.target.files[0])
-                       
-                       
-                      }  />
-          </div>
-       
+
+        <div>
+          <TextField
+            name="title"
+            label="title"
+            variant="filled"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          /><br />
+          <TextField
+            name="price"
+            label="price"
+            variant="filled"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+          />
+          <br />
+          <TextField
+            name="category"
+            label="category"
+            variant="filled"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          /><br />
+          <TextField
+            name="description"
+            label="description"
+            variant="filled"
+            multiline
+
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          /><br />
+          <TextField
+            name="image"
+            label="Image URL"
+            variant="filled"
+            multiline
+
+            value={image}
+            onChange={(e) => setImage(e.target.value)}
+          /><br />
+          {/* <Input accept="image/jpg, image/jpeg, image/png" type="file"  name="image" onChange={(e) => setSelectedFile(e.target.files[0]) }  /> */}
+        </div>
+
         <Button
           className={classes.button}
-          variant="contained" 
-          color="primary" 
-          type="submit" 
-         
-          onClick={handleSubmit}
-        >Save</Button>
+          variant="contained"
+          color="primary"
+          type="submit"
 
-<Button
-          className={classes.button}
-          variant="contained" 
-          color="default" 
-          type="submit" 
-         
           onClick={handleSubmit}
-        >Cancel</Button>
+        >{loading ? <CircularProgress color="inherit" /> : 'Save'}</Button>
+
+        {loading ? null : <Button
+          className={classes.button}
+          variant="contained"
+          color="default"
+          type="submit"
+
+          onClick={handleSubmit}
+        >Cancel</Button>}
       </form>
     </Container>
   );
